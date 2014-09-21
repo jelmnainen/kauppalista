40b4bebb9ffedf247e0379af1f2948bd77aee318
Johannes Elmnäinen <johannes.elmnainen@helsinki.fi>
2014-09-21 22:57:45 +0300

    loginform

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,6 +1,13 @@
-
-<ul>
-    <?php foreach( $output as $item ){ ?>
-        <li><?php echo $item ?></li>
-    <?php } ?>
-</ul>
+<form action="?page=login&action=login" method="post">
+    <ul>
+        <li>
+            Käyttäjätunnus: <input type="text" name="username">
+        </li>
+        <li>
+            Salasana: <input type="password" name="password">
+        </li>
+        <li>
+            <input type="submit" value="Lähetä">
+        </li>
+    </ul>
+</form>
