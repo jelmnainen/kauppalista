5aec4c8d0d454380259adb83e3e027ab1a4afbbb
Johannes Elmnäinen <johannes.elmnainen@helsinki.fi>
2015-01-08 14:58:12 +0200

    Switch home to use service/domain

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -1,36 +1,20 @@
 <?php
 
+/**
+ * This controller handles the general pages, such as landingpage
+ * and info page
+ */
 class home extends controllerbase{
     
+    /**
+     *Landing page is a static page at this point so.
+     */
     protected function index(){
-        $output = $this->model->index();
-        $this->display($output, true);
+        $this->display();
     }  
     
     protected function info(){
-        $output = $this->model->info();
-        $this->display($output);
-    }
-    
-    protected function login(){
-        $output = $this->model->login();
-        $this->display($output, true);
-    }
-    
-    //test if login script denies access to non-logged-in visitors
-    protected function membersonly(){
-        
-        if($this->user_is_logged_in()){
-          
-            $output = $this->model->membersonly();
-            $this->display($output, true);
-
-        } else {
-
-            $this->show_loginpage();
-        
-        }
-        
+        $this->display();
     }
     
 }
