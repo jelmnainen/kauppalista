ba50c0550a81190e252fb9fdedd32cdb385dff8b
Johannes Elmnäinen <johannes.elmnainen@helsinki.fi>
2015-01-08 17:07:02 +0200

    Registeration result more user-friendly

--- app/views/register/processRegisteration.php
+++ app/views/register/processRegisteration.php
@@ -1,2 +1,21 @@
-<h2><?php echo $output; ?></h2>
+
+<?php
+
+if($model["success"]){
+    
+    ?>
+
+        <h2>Rekisteröinti onnistui, tervetuloa!</h2>
+
+    <?php
+    
+    } else {
+        
+        ?>
+        
+        <h2>Rekisteröinti epäonnistui :( Kokeile uudestaan?</h2>
+        
+        <?php
+        
+    }
 
